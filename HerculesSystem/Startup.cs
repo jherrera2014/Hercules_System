35b171a4dca09efc2705e39952ea964debb8e1c1
jherrera2014 <jherrera@dnkwater.com>
2014-12-03 14:56:28 -0400

    Primero

---
+++ HerculesSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HerculesSystem.Startup))]
+namespace HerculesSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
